a71b7a046bc3807e54b03ee218411d054e507f37
Harry <habs@sdf.org>
2023-03-02 16:59:51 -0500

    bigger avatars

--- src/AthleteCard.tsx
+++ src/AthleteCard.tsx
@@ -184,7 +184,7 @@ export function AthleteCard({ avatar, name, job, stats, event, meet, entrant }: 
         }}
       >
         {/* <Card.Section sx={{ backgroundImage: `url(${image})`, height: 140 }} /> */}
-        <Avatar src={avatar} size={80} radius={80} mx="auto" className={classes.avatar} />
+        <Avatar mt={-20} src={avatar} size={128} radius={128} mx="auto" className={classes.avatar} />
         <Text align="center" size="lg" weight={500} mt="sm">
           {name}
         </Text>
